c2ca3743d495ea5328640469242f1a2aa7bb0780
Tal Yechye <taly@agrint.net>
2024-01-11 11:29:41 +0200

    Update dependencies and fix module paths and angular to 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { DatetimerangepickerModule } from 'angular-datetimerangepicker';
+import { DatetimerangepickerModule } from '../../projects/angular-datetimerangepicker/src/public_api';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
